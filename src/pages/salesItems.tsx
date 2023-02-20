a982254f51a0830e8504c4af883c10e75bf34019
keane <keanetay@gmail.com>
2023-02-20 14:28:50 +0800

    add upload fab

--- src/pages/salesItems.tsx
+++ src/pages/salesItems.tsx
@@ -1,8 +1,15 @@
 
 import TableComponent from '../components/Table-Component'
+import Fab from '@mui/material/Fab';
+import AttachFileIcon from '@mui/icons-material/AttachFile';
 
 export default function SalesItems() {
     return (
-        <TableComponent />
+        <div>
+            <TableComponent />
+            <Fab color="primary" aria-label="add">
+                <AttachFileIcon />
+            </Fab>
+        </div>
     )
 }
