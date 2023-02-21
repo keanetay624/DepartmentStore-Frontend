899c8cd41b63ac7f3d6ce788a953711566951efa
keane <keanetay@gmail.com>
2023-02-21 17:42:00 +0800

    move search field out of table comp

--- src/pages/salesItems/SalesItems.tsx
+++ src/pages/salesItems/SalesItems.tsx
@@ -1,13 +1,17 @@
 
 import TableComponent from './components/SalesItemTable'
+import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import { uploadSalesItems } from '../../util/ApiUtil'
 import { useState } from 'react'
 import UploadMessage from '../../components/UploadMessage';
+import Container from '@mui/system/Container';
 
 export default function SalesItems() {
     const [file, setFile] = useState<File>()
     const [inUploadProgress, setInUploadProgress] = useState(false)
+    const [searchVal, setSearchVal] = useState('')
+    const [searchStr, setSearchStr] = useState('')
 
     const handleFileChange = (e:any): void => {
         const uploadedFile = e.target.files[0]
@@ -21,15 +25,28 @@ export default function SalesItems() {
                 result => setInUploadProgress(false)
         );
     }
+
+    const handleSearchChange = (event:any) => {
+        setSearchVal(event.target.value);
+    };
+
+    const handleSearch = () => {
+        setSearchStr(searchVal);
+    };
+
      return (
         <div style={{width:"100%"}}>
-            <TableComponent />
-            <Button variant="contained" component="label">
-                Choose File
-                <input hidden type="file" onChange={handleFileChange}/>
-            </Button>
-            <Button onClick={handleFileUpload}>Upload</Button>
-            {inUploadProgress && UploadMessage("Upload in progress...")}  
+            <Container>
+                <TextField id="searchField" label="Search Items" variant="outlined" onChange={handleSearchChange}/>
+                <Button variant="contained" onClick={handleSearch}>Search</Button>
+                <Button variant="contained" component="label">
+                    Choose File
+                    <input hidden type="file" onChange={handleFileChange}/>
+                </Button>
+                <Button onClick={handleFileUpload}>Upload</Button>
+                {inUploadProgress && UploadMessage("Upload in progress...")}  
+            </Container>
+            <TableComponent searchStr={searchStr}/>
         </div>
     )
 }
--- src/pages/salesItems/components/SalesItemTable.tsx
+++ src/pages/salesItems/components/SalesItemTable.tsx
@@ -1,50 +1,39 @@
 import * as React from 'react';
 import { useState, useEffect } from 'react'
 import { DataGrid } from '@mui/x-data-grid';
-import TextField from '@mui/material/TextField';
 import Box from '@mui/material/Box';
 import SalesItemData from '../../../assets/DataInterface'
 import { getSalesItems } from '../../../util/ApiUtil'
 import columns from './SalesItemTableColumns'
 import '../../../App.css'
 import getSalesItemsArray from './getSalesItemsArray';
-import { Button } from '@mui/material';
 
-export default function TableComponent() {
+export default function TableComponent(props:any) {
   const [page, setPage] = React.useState(0);
   const [pageSize, setPageSize] = React.useState(5);
   const [rows, setRows] = useState<SalesItemData[]>([])
   const [totalSearchResults, setTotalSearchResults] = useState(0)
-  const [searchVal, setSearchVal] = useState('');
-  const [searchStr, setSearchStr] = useState('');
-
-  const handleSearchChange = (event:any) => {
-    setSearchVal(event.target.value);
-  };
-
-  const handleSearch = () => {
-    setSearchStr(searchVal);
-    setPage(0)
-  };
+  const [prevSearchStr, setPrevSearchStr] = React.useState('');
+  const searchStr = props.searchStr
 
   useEffect(() => {
+    if (searchStr !== prevSearchStr) setPage(0)
     const requestParams = {
       searchStr: searchStr,
       limit: pageSize,
       offset: page,
     }
-      getSalesItems(requestParams).then(
-        res => {
-          let salesItemsArray: SalesItemData[] = getSalesItemsArray(res.data)
-          setTotalSearchResults(res.data.totalSearchCount)
-          setRows(salesItemsArray)
-        })
+    getSalesItems(requestParams).then(
+      res => {
+        let salesItemsArray: SalesItemData[] = getSalesItemsArray(res.data)
+        setTotalSearchResults(res.data.totalSearchCount)
+        setRows(salesItemsArray)
+      })
+    setPrevSearchStr(searchStr)
   }, [page, pageSize, searchStr]) 
 
   return (
     <Box className="salesItemTable">
-      <TextField id="searchField" label="Search Items" variant="outlined" onChange={handleSearchChange}/>
-      <Button variant="contained" onClick={handleSearch}>Search</Button>
       <DataGrid
         rows={rows}
         columns={columns}
